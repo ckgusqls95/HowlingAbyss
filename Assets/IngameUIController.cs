78a848383813f8402498b8a9ef9d35cef6c1bbd7
chahyeonbeen <ckgusqls00@gmail.com>
2021-03-12 16:26:38 +0900

    GabrielAguiarProductions

---
+++ Assets/IngameUIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameUIController : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            PopupShop();
+        }
+    }
+
+    public void PopupShop()
+    {
+        GameObject shop = this.transform.Find("Shop").gameObject;
+
+        if(shop)
+        {
+            bool button = shop.activeInHierarchy == true ? false : true;
+            shop.SetActive(button);
+        }
+
+    }
+}
